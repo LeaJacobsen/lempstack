3eba9fbdf487af002a6ad815f2cc8a14cb5d30ae
Lea <lea@localhost>
2026-09-18 14:56:06 +0200

    Add NSG with HTTP and SSH-restricted rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,33 @@ resource "azurerm_subnet" "lamp" {
   virtual_network_name = azurerm_virtual_network.lamp.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "lamp" {
+  name                = "lamp-nsg"
+  location            = azurerm_resource_group.lamp.location
+  resource_group_name = azurerm_resource_group.lamp.name
+
+  security_rule {
+    name                       = "Allow-HTTP-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH-AdminOnly"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "185.73.72.112/32"
+    destination_address_prefix = "*"
+  }
+}
