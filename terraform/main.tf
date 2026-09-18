843fd5348f81ca1654c2e6511c36bc0d86154a51
Lea <lea@localhost>
2026-09-18 14:50:21 +0200

    Add VNet and subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,17 @@ resource "azurerm_resource_group" "lamp" {
   name     = "lamp-rg"
   location = "swedencentral"
 }
+
+resource "azurerm_virtual_network" "lamp" {
+  name                = "lamp-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.lamp.location
+  resource_group_name = azurerm_resource_group.lamp.name
+}
+
+resource "azurerm_subnet" "lamp" {
+  name                 = "lamp-subnet"
+  resource_group_name  = azurerm_resource_group.lamp.name
+  virtual_network_name = azurerm_virtual_network.lamp.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
